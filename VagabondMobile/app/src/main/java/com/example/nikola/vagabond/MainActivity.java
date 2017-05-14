35170d08dc2010e5d5d87142652a1e4663c614be
HomeN\Nikola <94nzmz94@gmail.com>
2017-05-14 11:16:01 +0200

    Simple android app

---
+++ VagabondMobile/app/src/main/java/com/example/nikola/vagabond/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.nikola.vagabond;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView)findViewById(R.id.wv);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+        });
+        webView.loadUrl("file:///android_asset/www/index.html");
+    }
+}
